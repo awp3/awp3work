283f554c883ae2eeb7175f119392eb1661731a44
Ahart Powers <apowersiii@me.com>
2018-03-31 19:31:57 +0900

    got the navbar to work correctly

--- src/js/PortfolioApp.js
+++ src/js/PortfolioApp.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Grid, Row, Col } from "react-bootstrap";
 import "../css/PortfolioApp.css";
 import LogoIcon from "./LogoIcon";
 import MyNavbar from "./Navbar";
@@ -34,11 +35,66 @@ class PortfolioApp extends Component {
 
   render() {
     return (
-      <div className="position">
-        <LogoIcon myLogo={"myLogo.svg"} />
-        <MyNavbar workPage={this.workPage} aboutMe={this.aboutMe} />
-        {this.currentView}
-      </div>
+      // <div className="position">
+      //   <LogoIcon myLogo={"myLogo.svg"} />
+      //   <MyNavbar workPage={this.workPage} aboutMe={this.aboutMe} />
+      //   {this.currentView}
+      // </div>
+
+      <Grid>
+        {/* my icon */}
+        <Row className="show-grid">
+          <Col xs={4} sm={4} md={4} />
+          <Col xs={4} sm={4} md={4}>
+            <LogoIcon myLogo={"myLogo.svg"} />
+          </Col>
+          <Col xs={4} sm={4} md={4} />
+        </Row>
+
+        {/* the navbar */}
+        <Row className="show-grid">
+          <Col
+            xs={2}
+            sm={2}
+            md={2}
+            // style={{ background: "pink", border: "1px solid purple" }}
+          >
+            {/* <code>&lt;{"Col xs={1} sm={2} md={2}"} /">">">">">">">">&gt;</code> */}
+          </Col>
+          <Col
+            xs={8}
+            sm={8}
+            md={8}
+            // style={{ background: "grey", border: "1px solid black" }}
+          >
+            <MyNavbar workPage={this.workPage} aboutMe={this.aboutMe} />
+            {/* <code>&lt;{"Col xs={8} sm={8} md={8}"} /">">">">">">">">&gt;</code> */}
+          </Col>
+          <Col
+            xs={2}
+            sm={2}
+            md={2}
+            // style={{ background: "pink", border: "1px solid purple" }}
+          >
+            {/* <code>&lt;{"Col xs={1} sm={2} md={2}"} /">">">">">">">">&gt;</code> */}
+          </Col>
+        </Row>
+
+        {/* <Row className="show-grid">
+          <Col xs={6} xsOffset={6}>
+            <code>&lt;{"Col xs={6} xsOffset={6}"} /">">">">">">">">&gt;</code>
+          </Col>
+        </Row> */}
+
+        {/* <Row className="show-grid">
+          <Col md={6} mdPush={9}>
+            <code>&lt;{"Col md={6} mdPush={9}"} /">">">">">">">">&gt;</code>
+          </Col>
+          <Col md={6} mdPull={6}>
+            <code>&lt;{"Col md={6} mdPull={6}"} /">">">">">">">">&gt;</code>
+          </Col>
+        </Row> */}
+      </Grid>
     );
   }
 }
--- src/js/Thumbnail.js
+++ src/js/Thumbnail.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
+import { Thumbnail } from "react-bootstrap";
 import "../css/Thumbnail.css";
 
-export default class Thumbnail extends Component {
+export default class MyThumbnail extends Component {
   render() {
     return (
+      // <Thumbnail className="thumbnailSize">
+      //   <div className="mockImage">
+      //     <img src={this.props.thumbnail} alt={this.props.title} />
+      //   </div>
+      //   <h3 className="text">{this.props.title}</h3>
+      // </Thumbnail>
+
       <div className="thumbnailSize" onClick={this.props.showMe}>
         <div className="mockImage">
           <img src={this.props.thumbnail} alt={this.props.title} />
